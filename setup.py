5c486ce8061a98114c9e811ebfbe140acaf80d8f
nac5504 <ncandello3@gatech.edu>
2026-01-07 23:41:02 -0500

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="copylab",
-    version="1.0.1",
+    version="1.0.2",
     description="CopyLab Python SDK for secure notification management",
     author="CopyLab",
     package_dir={"": "Sources"},
